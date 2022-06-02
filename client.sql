57f2280b9b714844aa8102523ed16fb2a798ea31
Mickaël Véril <mika.veril@wanadoo.fr>
2022-06-02 10:58:57 +0200

    Creation of the DATABASE

---
+++ client.sql
@@ -0,0 +1,20 @@
+-- authentification
+
+>mysql -u root -- si l'utilisateur n'a pas de mot de passe
+
+>mysql -u root -p -- si l'utilisateur n'a pas de mot de passe
+
+-- Lister toutes les bases de donnée
+
+SHOW DATABASES.
+
+-- Création de la base de donnée
+
+CREATE DATABASE ventes;
+
+
+
+-- Création de la table client
+
+
+CREATE TABLE client
